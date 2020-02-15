557178f40077d9143a4290f92558a9eb8cb3cfb9
rodmasson <rodmasson@gmail.com>
2020-02-14 22:52:47 -0300

    Corda_Aula140220

---
+++ workflows/src/main/java/com/template/flows/VerifySignAndFinalizeFlow.java
@@ -0,0 +1,8 @@
+package com.template.flows;
+
+import net.corda.core.transactions.TransactionBuilder;
+
+public class VerifySignAndFinalizeFlow {
+    public VerifySignAndFinalizeFlow(TransactionBuilder builder) {
+    }
+}
